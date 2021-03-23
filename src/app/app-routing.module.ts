8757d0d6f5e347ae9f0ba0a2498c22ee949bbacb
Bexaraya Ahmed <bahmed@gfi.es>
2021-03-23 16:17:18 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForgotPasswordComponent } from './callbacks/forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './callbacks/page-not-found/page-not-found.component';
+import { DoctorComponent } from './doctors/doctor.component';
+import { HospitalComponent } from './hospitals/hospital.component';
+import { LoginComponent } from './login/login.component';
+import { PatientComponent } from './patients/patient.component';
+import { UserRegistrationComponent } from './registration/user-registration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: UserRegistrationComponent, data: {title: 'Registro usuario nuevo'}},
+  { path: 'forgotpassword', component: ForgotPasswordComponent, data: {title: 'Password olvidao'}},
+  { path: 'login', component: LoginComponent, data: {title: 'Login usuario'}},
+  
+  { path: 'doctor/:id', component: DoctorComponent},
+  { path: 'patient/:id', component: PatientComponent},
+  { path: 'hospital/:id', component: HospitalComponent},
+  
+  { path: 'index', component: AppComponent},
+  { path: '', redirectTo: '/index', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { UserRegistrationComponent } from './registration/user-registration.component';
+import { DoctorComponent } from './doctors/doctor.component';
+import { PatientComponent } from './patients/patient.component';
+import { HospitalComponent } from './hospitals/hospital.component';
+import { ForgotPasswordComponent } from './callbacks/forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './callbacks/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserRegistrationComponent,
+    DoctorComponent,
+    PatientComponent,
+    HospitalComponent,
+    ForgotPasswordComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
