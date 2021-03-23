3950215360809cdae61a7c11c238a2017d9a7dea
Bexaraya Ahmed <bahmed@gfi.es>
2021-03-23 11:15:42 +0100

    Init reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 import { Login } from './login';
 
 @Component({
@@ -8,16 +10,70 @@ import { Login } from './login';
 })
 export class LoginComponent implements OnInit {
 
-  model = new Login('', '');
+  formErros = {
+    'email': '',
+    'password': ''
+  };
 
-  constructor() { }
+  validationMessages = {
+    'email': {
+      'required': 'El email es requerido.'
+    },
+    'password': {
+      'required': 'La contraseña es requerida.'
+    }
+  };
 
-  ngOnInit() {
+  loginForm: FormGroup;
+
+  login = new Login('', '');
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
   onSubmit() {
-    console.log('Email address: ' + this.model.email + ', Password: '
-      + this.model.password)
+    console.log('Email address: ' + 
+      this.loginForm.get('email').value + ', Password: ' +
+      this.loginForm.get('password').value)
+    this.login = this.loginForm.value;
+    console.log('Email address: ' + this.login.email + ', Password: ' +
+                this.login.password)
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: [this.login.email, Validators.required],
+      password: [this.login.password, Validators.required]
+    });
+
+    this.loginForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onValueChanged(data?: any): void {
+    if (!this.loginForm) {
+      return;
+    }
+    const form = this.loginForm;
+
+    for (const field in this.formErros) {
+      // clear previous error message (if any)
+      this.formErros[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErros[field] +=  messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  ngOnInit() {
   }
 
 }
